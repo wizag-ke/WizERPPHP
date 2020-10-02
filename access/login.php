1abcd50f29045b723e56e8d21e7b7fcc3c5acc77
Kinyua Mwaniki <kinyuafelix4@gmail.com>
2020-10-02 09:48:29 +0300

    Login page updated

--- access/login.php
+++ access/login.php
@@ -28,7 +28,7 @@ function defaultCompany()
 	}
 	else
 	{
-		$demo_text = _("Please login here");
+		// $demo_text = _("Please login here");
     	if (@$SysPrefs->allow_password_reset) {
       		$demo_text .= " "._("or")." <a href='$path_to_root/index.php?reset=1'>"._("request new password")."</a>";
     	}
@@ -77,22 +77,17 @@ function defaultCompany()
 	br();br();
 
 	echo '<div class="card mx-auto" style="width: 30rem;">';
-	echo '<img class="card-img-top" src="'.$path_to_root.'/themes/'.$def_theme.'/images/erp.png" alt="Card image cap">';
-	// echo '<ul class="list-group list-group-flush">';
-	// echo '<li class="list-group-item">Cras justo odio</li>';
-	// echo '<li class="list-group-item">Dapibus ac facilisis in</li>';
-	// echo '<li class="list-group-item">Vestibulum at eros</li>';
-	// echo '</ul>';
+	echo '<img class="card-img-top mx-auto mt-2" style="width:90%" src="'.$path_to_root.'/themes/'.$def_theme.'/images/erp.png" alt="Card image cap">';
 	echo '<div class="card-body">';
 
 
 	start_form(false, false, $_SESSION['timeout']['uri'], "loginform");
 	start_table(false, "class='login'");
 	start_row();
-	echo "<td align='center' colspan=2>";
+	// echo "<td align='center' colspan=2>";
 	if (!$login_timeout) { // FA logo
     	// echo "<a target='_blank' href='".$SysPrefs->power_url."'><img src='$path_to_root/themes/$def_theme/images/erp.png' alt='FrontAccounting' height='50' onload='fixPNG(this)' border='0' ></a>";
-    	echo "<a target='_blank' href='".$SysPrefs->power_url."'><img src='$path_to_root/themes/$def_theme/images/erp.png' alt='FrontAccounting' height='50' border='0' ></a>";
+    	// echo "<a target='_blank' href='".$SysPrefs->power_url."'><img src='$path_to_root/themes/$def_theme/images/erp.png' alt='FrontAccounting' height='50' border='0' ></a>";
 	} else { 
 		echo "<font size=5>"._('Authorization timeout')."</font>";
 	} 
@@ -107,7 +102,7 @@ function defaultCompany()
 
 	$password = $SysPrefs->allow_demo_mode ? "password":"";
 
-	password_row(_("Password:"), 'password', $password);
+	password_row(_("Password"), 'password', $password);
 
 
 	if ($login_timeout) {
@@ -117,7 +112,7 @@ function defaultCompany()
 		if (!isset($coy))
 			$coy = $def_coy;
 		if (!@$SysPrefs->text_company_selection) {
-			echo "<tr><td>"._("Company")."</td><td><select name='company_login_name'>\n";
+			echo "<tr><td class='label'>"._("Company")."</td><td><select name='company_login_name'>\n";
 			for ($i = 0; $i < count($db_connections); $i++)
 				echo "<option value=$i ".($i==$coy ? 'selected':'') .">" . $db_connections[$i]["name"] . "</option>";
 			echo "</select>\n";
@@ -126,12 +121,12 @@ function defaultCompany()
 			text_row(_("Company"), "company_login_nickname", "", 20, 50);
 		}
 		start_row();
-		label_cell($demo_text, "colspan=2 align='center' id='log_msg'");
+		// label_cell($demo_text, "colspan=2 align='center' id='log_msg'");
 		end_row();
 	}; 
 	end_table(1);
 	echo "<input type='hidden' id=ui_mode name='ui_mode' value='".!fallback_mode()."' >\n";
-	echo "<center><input type='submit' value='&nbsp;&nbsp;"._("Login -->")."&nbsp;&nbsp;' name='SubmitUser'"
+	echo "<center><input class='btn btn-block login-button' type='submit' value='&nbsp;&nbsp;"._("Sign in")."&nbsp;&nbsp;' name='SubmitUser'"
 		." onclick='set_fullmode();'".(isset($blocked_msg) ? " disabled" : '')." ></center>\n";
 
 	foreach($_SESSION['timeout']['post'] as $p => $val) {
