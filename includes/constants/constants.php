ebf4a748d315e9f61a2b0b3767b231d9b693fdf0
Kinyua Mwaniki <kinyuafelix4@gmail.com>
2020-10-30 08:44:44 +0300

    Transaction mapping checks to see if mapping already exists before it saves

--- includes/constants/constants.php
+++ includes/constants/constants.php
@@ -1,8 +1,5 @@
 <?php
 
 const MODULES = [
-    'Good received notes' => 'Good received notes', 
     'Internal GRN' => 'Internal GRN', 
-    'Credit notes' => 'Credit notes',
-    'Debit notes' => 'Debit notes',
 ];
