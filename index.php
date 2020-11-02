f3e696596e9d553eb593b6901ce154e39dfd9070
Kinyua Mwaniki <kinyuafelix4@gmail.com>
2020-11-02 09:58:28 +0300

    commit development

--- index.php
+++ index.php
@@ -9,17 +9,17 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
     See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
 ***********************************************************************/
-	$path_to_root=".";
-	if (!file_exists($path_to_root.'/config_db.php'))
-		header("Location: ".$path_to_root."/install/index.php");
+$path_to_root=".";
+if (!file_exists($path_to_root.'/config_db.php'))
+	header("Location: ".$path_to_root."/install/index.php");
 
-	$page_security = 'SA_OPEN';
-	ini_set('xdebug.auto_trace',1);
-	include_once("includes/session.inc");
+$page_security = 'SA_OPEN';
+ini_set('xdebug.auto_trace',1);
+include_once("includes/session.inc");
 
-	add_access_extensions();
-	$app = &$_SESSION["App"];
-	if (isset($_GET['application']))
-		$app->selected_application = $_GET['application'];
+add_access_extensions();
+$app = &$_SESSION["App"];
+if (isset($_GET['application']))
+	$app->selected_application = $_GET['application'];
 
-	$app->display();
+$app->display();
