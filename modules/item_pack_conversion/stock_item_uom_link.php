e87c991ea3ba821aed01165728c849f5b2b4121c
Kinyua Mwaniki <kinyuafelix4@gmail.com>
2020-11-03 12:09:31 +0300

    Created stock item to uom link form

--- modules/item_pack_conversion/stock_item_uom_link.php
+++ modules/item_pack_conversion/stock_item_uom_link.php
@@ -20,4 +20,12 @@ include_once($path_to_root . "/modules/item_pack_conversion/includes/item_pack_c
 
 page(_("Stock item to UOM link"), false, false, "", $js);
 
+
+start_form();
+start_table();
+stock_items_list_row_conversion(_("Item:"), 'stock_code', $_POST['stock_code']);
+gl_all_uoms_list_row(_("UOM:"), 'uom_id', $_POST['uom_id']);
+end_table(1);
+submit_add_or_update_center($selected_id == -1, '', 'both');
+end_form();
 end_page();
